ec1b7583df5069ea007497563366ad2aa49704ce
opsb <oliver@opsb.co.uk>
2010-09-16 12:47:58 +0100

    Added handler for creating new databases

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem 'sinatra', '1.0'
+gem 'sequel', '3.15.0'
+gem 'mysql', '2.8.1'
---
+++ lib/dbyard.rb
@@ -0,0 +1,39 @@
+require "rubygems"
+require "bundler"
+Bundler.setup
+require 'sinatra'
+require 'digest'
+require 'sequel'
+
+DB = Sequel.connect(ENV['DBYARD_DB_URI'])
+
+get '/' do
+  "dbyard"
+end
+
+get '/create' do
+  create_db
+end
+
+post '/' do
+  create_db
+end
+
+def create_db
+  uuid = Digest::SHA1.hexdigest(Time.now.to_s)
+  puts uuid
+  schema = "s" + uuid
+  username = "u" + uuid[0,14]
+  password = "p" + uuid
+  dbrun("create schema #{schema}")
+  dbrun("create user #{username} identified by password '#{password}'")
+  permissions = "alter, create, create temporary tables, delete, drop, index, insert, lock tables, select, update"
+  dbrun("grant #{permissions} on #{schema}.* to #{username}@'%' identified by '#{password}'")
+  dbrun("flush privileges")
+  "mysql -u #{username} --password=#{password} -h #{request.host} #{schema}"
+end
+
+def dbrun(command)
+  puts command
+  DB.run(command)
+end
